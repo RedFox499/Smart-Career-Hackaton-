1d1ffc0885d3a4a458d676263d53cfe735fac06f
AlexFIDA <aleksandrpapasvili505@gmail.com>
2026-02-25 12:15:31 +0500

    Add apps in settings.py

--- Smart_Career/Smart_Career/settings.py
+++ Smart_Career/Smart_Career/settings.py
@@ -47,6 +47,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users',
+    'ai_core',
+    'career'
 ]
 
 ROOT_URLCONF = 'Smart_Career.urls'
